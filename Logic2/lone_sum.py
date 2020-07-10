a00846dd8b6fa35149720c1e71558529e50604ec
sayak-3848 <67751201+sayak-3848@users.noreply.github.com>
2020-07-11 01:21:48 +0530

    codingbat python solutions

---
+++ Logic2/lone_sum.py
@@ -0,0 +1,18 @@
+## Given 3 int values, abc, return their sum. However,
+## if one of the values is the same as another of the
+## values, it does not count towards the sum.
+
+def lone_sum(a, b, c):
+    s = 0
+    if a != b and a != c:
+        s += a
+    if b != a and b != c:
+        s += b
+    if c != a and c != b:
+        s += c
+    return s
+
+print(lone_sum(1,2,3))
+print(lone_sum(3,2,3))
+print(lone_sum(3,3,3))
+
